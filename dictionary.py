fb9712c9ff484add0573e5144f6741d27253f1a9
Hannafaith B. Punzal <92629367+hannapunzal@users.noreply.github.com>
2022-11-12 11:51:15 +0800

    Implemented options 2 and 3, search and exit

--- dictionary.py
+++ dictionary.py
@@ -1,31 +1,37 @@
-# display menu
-print("""----- M E N U -----\n
-Option 1: Add a contact information
-Option 2: Search for existing contact
-Option 3: Exit program\n
--------------------
-""")
+while True:
 
-# allow user to select (check if valid)
-userOption = int(input("Choose from the options above [1-3]: "))
+    # display menu
+    print("""----- M E N U -----\n
+    Option 1: Add a contact information
+    Option 2: Search for existing contact
+    Option 3: Exit program\n
+    -------------------
+    """)
 
-#option 1: personal data
-if userOption == 1:
-    name = input("Enter your full name: ")
-    age = int(input("Enter your age: "))
-    address = input("Enter your address: ")
-    phone = int(input("Enter your phone number: "))
-    vax = input("Are you vaccinated against COVID-19? [yes/no]: ")
+    # allow user to select (check if valid)
+    userOption = int(input("Choose from the options above [1-3]: "))
 
-# store user info
-contactTracing = {
-    name: {
-        age,
-        address,
-        phone,
-        vax
+    #option 1: personal data
+    if userOption == 1:
+        inputName = input("Enter your full name: ")
+        name = inputName.upper()
+        age = int(input("Enter your age: "))
+        address = input("Enter your address: ")
+        phone = int(input("Enter your phone number: "))
+        vax = input("Are you vaccinated against COVID-19? [yes/no]: ")
+
+    # store user info
+    contactTracing = {
+        name: [age,address,phone,vax]
     }
-}
-print(contactTracing)
-# option 2: search full name, display
-# option 3 exit
+
+    # option 2: search full name, display
+    if userOption == 2:
+        search = input("Enter the full name of contact: ")
+        contactSearch = search.upper()
+        if contactSearch in contactTracing:
+            print("Here's the contact information you're searching for: ", contactTracing[contactSearch])
+
+    # option 3 exit
+    if userOption == 3:
+        break
