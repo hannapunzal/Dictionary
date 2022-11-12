9156180690701171056667747408b63552437c98
Hannafaith B. Punzal <92629367+hannapunzal@users.noreply.github.com>
2022-11-12 12:50:39 +0800

    Implemented new way for accessing and displaying dictionary values.

--- dictionary.py
+++ dictionary.py
@@ -10,13 +10,15 @@ while True:
     # allow user to select (check if valid)
     userOption = int(input("Choose from the options above [1-3]: "))
 
-    if userOption >3 or userOption <1:
+    if userOption !=1 and userOption!=2 and userOption!=3:
         print("Sorry, you entered an invalid option. Please choose from 1-3 only: ")
-
-    #option 1: personal data
+        print()
+    
+     #option 1: personal data
     if userOption == 1:
         inputName = input("Enter your full name: ")
         name = inputName.upper()
+        gender = input("Enter your gender [f/m/rather not say)]: ")
         age = int(input("Enter your age: "))
         address = input("Enter your address: ")
         phone = int(input("Enter your phone number: "))
@@ -26,7 +28,12 @@ while True:
 
     # store user info
         contactTracing = {
-            name: [inputName,age,address,phone,vax]
+            name: {"Name": inputName,
+                   "Gender" : gender,
+                   "Age" : age,
+                   "Address" : address,
+                   "Phone" :phone,
+                   "Vax" :vax}
         }
 
     # option 2: search full name, display
@@ -35,14 +42,15 @@ while True:
         print()
         contactSearch = search.upper()
         if contactSearch in contactTracing:
-            print("Here's the contact information you're searching for:\n","Full name, Age, Address, Phone number, Vaccination status:\n", contactTracing[contactSearch])
+            print("Here's the contact information you're searching for:\n")
+            for key, value in contactTracing.items():
+                print("Name: ",value["Name"],"\nGender: ",value["Gender"],"\nAge: ",value["Age"], "\nAddress: ",value["Address"],"\nPhone number: ",value["Phone"],"\nVaccination status: ",value["Vax"])
             print()
         if contactSearch not in contactTracing:
             print("Sorry, you entered a non-existent contact.\n Choose Option 1 if you want to create\n a new contact.\n")
-            
+                
 
     # option 3 exit
     if userOption == 3:
         print("\nThank you for using HF's Contact Tracer!")
         break
-   
