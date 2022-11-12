80b023bc6e27cb9c36355fb47e92b22a750825d8
Hannafaith B. Punzal <92629367+hannapunzal@users.noreply.github.com>
2022-11-12 11:22:55 +0800

    Created pseudocode and implemented Display Menu

---
+++ dictionary.py
@@ -0,0 +1,11 @@
+# display menu
+print("""----- M E N U -----\n
+Option 1: Add a contact information
+Option 2: Search for existing contact
+Option 3: Exit program\n
+-------------------
+""")
+# allow user to select (check if valid)
+#option 1: personal data
+#option 2: search full name, display
+# option 3 exit
